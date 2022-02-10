eed6f1ff57288e3a930ebc635d0b8411761f757f
krystel <krystel.dev@gmail.com>
2022-02-10 13:18:50 +0100

    init commit

---
+++ src/page/home/Home.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+import { Link } from "react-router-dom";
+
+const PokedexTemplate = () => {
+  return (
+    <Fragment>
+      <div>Home</div>
+    </Fragment>
+  );
+};
+export default PokedexTemplate;
